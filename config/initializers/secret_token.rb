8d769c0af6775d2255b33f787fd50b05e9b059fe
Charlie Bedard <charlie@cks-partners.com>
2012-12-30 14:53:27 -0800

    Initial checkin of base level code to begin the demo app development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c81aae75fe17e89bb17385eb085211e409631dff0bd4ec97cf2629e0906c494f2efd43e1798a38ad5d167c5af18b89fc783f881f981e44b5acc817ea82948846'
